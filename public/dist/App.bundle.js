6326661868eb91e029df0e8f47fde77b27d343d3
DavidThorpe71 <davidthorpe878@gmail.com>
2017-12-05 22:14:14 +0000

    Completed bootstrap layout

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -2448,7 +2448,6 @@ exports.default = initMap;
 var $grid = $('.grid').imagesLoaded(function () {
 		// init Masonry after all images have loaded
 		$grid.masonry({
-
 				// set itemSelector so .grid-sizer is not used in layout
 				itemSelector: '.grid-item',
 				// use element for option
--- public/javascripts/modules/masonry.js
+++ public/javascripts/modules/masonry.js
@@ -1,7 +1,6 @@
 const $grid = $('.grid').imagesLoaded( function() {
   // init Masonry after all images have loaded
   $grid.masonry({
-
   	// set itemSelector so .grid-sizer is not used in layout
 	itemSelector: '.grid-item',
 	// use element for option
